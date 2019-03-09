2efb5de2bb2d478880f2efe25c84b0479d8669c8
steve hanlon <rshanlon@gmail.com>
2019-03-09 11:42:45 -0800

    Refactored checkWinner function; added winningSMG function along with HTML and CSS for winning message

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ CodePen: [Code sample here.](https://codepen.io/SixStringsCoder/pen/ZwqKpJ?edito
 
 Goal: Recreate a web version of Tic-Tac-Toe.
 
+![UI Sample](./screenshots/screen_sample_winner.png)
+
+To-Do-List:
+- Add 'Play Again' button
+
 Extra features:
+- Connect to trivia question where correct answer gets to make a move or correct answer gets to move opponent to different space
+- Add option to select other symbols other than 'X' and 'O'
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const gameboard = document.getElementById('tictactoeContainer');
 const squares = document.querySelectorAll('.square');
+const winnerBoard = document.querySelector('#winnerBoard');
 const squareID = ["tl", "tc", "tr", "cl", "cc", "cr", "bl", "bc", "br"];
 let myTurn = true;
+let numberOfPLays = 0; // start comparing when > 4
 const boardValues = {
   tl: "",
   tc: "",
@@ -14,12 +16,25 @@ const boardValues = {
   br: ""
 };
 
-// Array of 8 possible winning combos
-    // from top left - 3 combos (index+1, +3, +4)
-    // from top center - 1 combo (+3)
-    // from top right - 2 combos (+2, +3)
-    // from center left - 1 combo (+1)
-    // from bottom left - 1 combo (+1)
+const boardIDs = ["tl", "tc", "tr", "cl", "cc", "cr", "bl", "bc", "br"];
+
+const winningCombos = [
+  ["tl", "tc", "tr"],
+  ["tl", "cl", "bl"],
+  ["tl", "cc", "br"],
+  ["tc", "cc", "br"],
+  ["tr", "cr", "br"],
+  ["tr", "cc", "bl"],
+  ["cl", "cc", "cr"],
+  ["bl", "bc", "br"]
+];
+
+// 8 possible winning combos
+    // from top left - 3 combos (["tl", "tc", "tr"], ["tl", "cl", "bl"], ["tl", "cc", "br"])
+    // from top center - 1 combo (["tc", "cc", "br"])
+    // from top right - 2 combos (["tr", "cr", "br"], ["tr", "cc", "bl"])
+    // from center left - 1 combo (["cl", "cc", "cr"])
+    // from bottom left - 1 combo (["bl", "bc", "br"])
 
 
 (function makeGameBoard() {
@@ -32,7 +47,6 @@ const boardValues = {
   }
 })();
 
-
 // Add X and O to board after clicking squares
 gameboard.addEventListener('click', (square) => {
   let sign = !myTurn ? "O" : "X";
@@ -40,15 +54,33 @@ gameboard.addEventListener('click', (square) => {
   document.getElementById(square.target.id).innerHTML = `<span class="xo-sign">${sign}</span>`;
   // Change myTurn boolean to opposite value of current value
   myTurn = !myTurn;
-  addBoardValue(square.target.id, sign);
+  // Keep track of where Xs and Os are; seen in console only
+  addBoardValues(square.target.id, sign);
+  numberOfPLays += 1;
 });
 
-const addBoardValue = (target, sign) => {
+const addBoardValues = (target, sign) => {
   boardValues[target] = sign;
-  console.log(boardValues);
   checkWinner(boardValues)
 }
 
-const checkWinner = (valueObject) => {
-    console.log(squares)
+const checkWinner = () => {
+  for (let i = 0; i < winningCombos.length; i++) {
+    const [ sp1, sp2, sp3 ] = winningCombos[i];
+    for (let key in boardValues) {
+      if (boardValues[sp1]
+         &&
+         boardValues[sp1] === boardValues[sp2]
+         &&
+         boardValues[sp2] === boardValues[sp3]) {
+          let winner = boardValues[sp1];
+          setTimeout(winngMsg, 1000, winner);
+          return boardValues[sp1];
+         }
+    }
+  }
+}
+
+const winngMsg = (winner) => {
+  winnerBoard.innerHTML = `<h1>Winner is <span id="winnerSymbol">${winner}</span></h1>`;
 }
